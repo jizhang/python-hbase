c5257e9947247ccaf8834354a4deaf976c7dbd0e
Ji ZHANG <zhangji87@gmail.com>
2018-04-21 19:38:34 +0800

    examples

--- common.py
+++ common.py
@@ -5,7 +5,6 @@ from thrift.transport import TSocket
 from thrift.protocol import TBinaryProtocol
 from thrift.transport import TTransport
 from hbase import THBaseService
-from hbase.ttypes import TGet
 
 
 @contextlib.contextmanager
@@ -18,10 +17,3 @@ def connect():
         yield client
     finally:
         transport.close()
-
-
-if __name__ == '__main__':
-    with connect() as client:
-        tget = TGet(row='sys.cpu.user:20180421:192.168.1.1')
-        tresult = client.get("tsdata", tget)
-        print(tresult)
